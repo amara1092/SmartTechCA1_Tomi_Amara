2defb07da9a6a7d9b8364d90bb8ec3e4653dd69e
tak <Takkarigomba@gmail.com>
2021-12-21 16:35:44 +0000

    print category variables and id

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import pickle
 import pandas as pd
 import cv2
 import os, os.path
+import json
 
 
 def gray_scale(img):
@@ -40,54 +41,75 @@ def preprocess(img):
 
 ##TRAIN##
 path='C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\test'
-images=os.listdir(path)
-type(images)
-len(images)
-
-img_data_train=[]
-for img in images:
-    img_arr=cv2.imread(os.path.join(path, img))
-    img_data_train.append(img_arr)
-
-for i in range(len(img_data_train)):
-    plt.imshow(img_data_train[i])
-    #plt.imshow(img, cmap=plt.get_cmap('gray'))
-    #plt.show()
-
-##TEST##
-#path
-path='C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\train'
-images=os.listdir(path)
-type(images)
-len(images)
-
-img_data_test=[]
-for img in images:
-    img_arr=cv2.imread(os.path.join(path,img))
-    img_data_test.append(img_arr)
-
-for i in range(len(img_data_test)):
-    plt.imshow(img_data_test[i])
-    #plt.imshow(img, cmap=plt.get_cmap('gray'))
-    #plt.show()
-
-##VALIDATION##
-path='C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\val'
-images=os.listdir(path)
-type(images)
-len(images)
-
-img_data_val=[]
-for img in images:
-    img_arr=cv2.imread(os.path.join(path,img))
-    img_data_val.append(img_arr)
-
-for i in range(len(img_data_val)):
-    plt.imshow(img_data_val[i])
-    img = preprocess(img_data_val[i])
-    plt.imshow(img, cmap=plt.get_cmap('gray'))
-    plt.axis("off")
-    plt.show()
-
-#data = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
+# images=os.listdir(path)
+# type(images)
+# len(images)
+#
+# img_data_train=[]
+# for img in images:
+#     img_arr=cv2.imread(os.path.join(path, img))
+#     img_data_train.append(img_arr)
+#
+# for i in range(len(img_data_train)):
+#     plt.imshow(img_data_train[i])
+#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
+#     #plt.show()
+#
+# ##TEST##
+# #path
+# path='C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\train'
+# images=os.listdir(path)
+# type(images)
+# len(images)
+#
+# img_data_test=[]
+# for img in images:
+#     img_arr=cv2.imread(os.path.join(path,img))
+#     img_data_test.append(img_arr)
+#
+# for i in range(len(img_data_test)):
+#     plt.imshow(img_data_test[i])
+#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
+#     #plt.show()
+#
+# ##VALIDATION##
+# path='C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\val'
+# images=os.listdir(path)
+# type(images)
+# len(images)
+#
+# img_data_val=[]
+# for img in images:
+#     img_arr=cv2.imread(os.path.join(path,img))
+#     img_data_val.append(img_arr)
+#
+# for i in range(len(img_data_val)):
+#     plt.imshow(img_data_val[i])
+#     img = preprocess(img_data_val[i])
+#    # plt.imshow(img, cmap=plt.get_cmap('gray'))
+#     plt.axis("off")
+#     #plt.show()
+data =[]
+data = pd.read_json('C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\bdd100k\\labels\\bdd100k_labels_images_val.json')
 #print(data)
+
+with open ('C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\bdd100k\\labels\\bdd100k_labels_images_val.json') as f:
+    data = json.load(f)
+   # print (data)
+
+li = [item.get('labels') for item in data]
+print(li)
+
+for item in data:
+    for data_item in item['labels']:
+        print (data_item['category'])
+
+for item in data:
+    for data_item in item['labels']:
+        print (data_item['id'])
+
+
+
+
+
+
