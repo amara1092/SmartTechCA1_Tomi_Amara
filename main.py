4dafdf3f20d987a3bcc270210f271acb28beb846
Amara <amara1092@gmail.com>
2021-12-22 21:13:41 +0000

    Labels Extraction and Resizing of images

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import pickle
 import pandas as pd
 import cv2
 import os, os.path
+import json
 
 
 def gray_scale(img):
@@ -31,49 +32,60 @@ def equalise(img):
     img = cv2.equalizeHist(img)
     return img
 
-
 def preprocess(img):
     img = gray_scale(img)
     img = equalise(img)
     img = img/255
     return img
 
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+
 ##TRAIN##
-path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\train10'
-images=os.listdir(path)
-type(images)
-len(images)
-
-img_data_train=[]
-for img in images:
-    img_arr=cv2.imread(os.path.join(path, img))
-    img_data_train.append(img_arr)
-
-for i in range(len(img_data_train)):
-    plt.imshow(img_data_train[i])
-    #plt.show()
-
-##TEST##
-#path
-path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\test10'
-images=os.listdir(path)
-type(images)
-len(images)
-
-img_data_test=[]
-for img in images:
-    img_arr=cv2.imread(os.path.join(path,img))
-    img_data_test.append(img_arr)
-
-for i in range(len(img_data_test)):
-    plt.imshow(img_data_test[i])
-    #plt.show()
-
+#path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\train10'
+# images=os.listdir(path)
+# type(images)
+# len(images)
+#
+# img_data_train=[]
+# for img in images:
+#     img_arr=cv2.imread(os.path.join(path, img))
+#     img_data_train.append(img_arr)
+#
+# for i in range(len(img_data_train)):
+#     plt.imshow(img_data_train[i])
+#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
+#     #plt.show()
+#
+# ##TEST##
+# #path
+#path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\test10'
+# images=os.listdir(path)
+# type(images)
+# len(images)
+#
+# img_data_test=[]
+# for img in images:
+#     img_arr=cv2.imread(os.path.join(path,img))
+#     img_data_test.append(img_arr)
+#
+# for i in range(len(img_data_test)):
+#     plt.imshow(img_data_test[i])
+#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
+#     #plt.show()
+#
 ##VALIDATION##
 path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\val10'
 images=os.listdir(path)
 type(images)
 len(images)
+img_size = 224
 
 img_data_val=[]
 for img in images:
@@ -83,9 +95,51 @@ for img in images:
 for i in range(len(img_data_val)):
     plt.imshow(img_data_val[i])
     img = preprocess(img_data_val[i])
-    plt.imshow(img)
+    img = cv2.resize(img, (img_size, img_size))
+    plt.imshow(img, cmap=plt.get_cmap('gray'))
     plt.axis("off")
-   # plt.show()
+    #plt.show()
 
+filename='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\car'
+data =[]
 data = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
-print(data)
+#print(data)
+
+with open ('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json') as f:
+    data = json.load(f)
+
+   # print (data)
+
+
+li = [item.get('labels') for item in data]
+#print(li)
+items = json.dumps(li)
+#print(type(items))
+
+for item in data:
+    for data_item in item['labels']:
+        data = (data_item['category'])
+        if 'car' in data :
+            a = item.get('name')
+            print(a)
+
+createFolder('./car/')
+cv2.imwrite('./car/', a)
+
+for box in data_item:
+    x1 = int(item['box2d']['x1'])
+    y1 = int(item['box2d']['y1'])
+    x2 = int(item['box2d']['x1'])
+    y2 = int(item['box2d']['y1'])
+    cropped_image = img[y1:y1 + y2, x1:x1 + x2]
+    x = np.append(x, preprocess(cropped_image))
+
+
+
+#
+# for item in data:
+#     for data_item in item['labels']:
+#         print (data_item['id'])
+
+
+
