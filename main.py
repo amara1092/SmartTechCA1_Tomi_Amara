6c939d678be75424d3fa3c7f033577ccdc50d5d1
Amara <amara1092@gmail.com>
2021-12-23 01:22:08 +0000

    Label extraction complete

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow.keras
 from keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
+import keras
 import random
 import requests
 from PIL import Image
@@ -46,100 +49,128 @@ def createFolder(directory):
     except OSError:
         print('Error: Creating directory. ' + directory)
 
+# def newlabels(labels):
+#   updlabels = pd.DataFrame.from_records(labels)
+#   updlabels = updlabels['category'].unique().tolist()
+#   return ','.join(updlabels)
+
 
 ##TRAIN##
-#path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\train10'
-# images=os.listdir(path)
-# type(images)
-# len(images)
-#
-# img_data_train=[]
-# for img in images:
-#     img_arr=cv2.imread(os.path.join(path, img))
-#     img_data_train.append(img_arr)
-#
-# for i in range(len(img_data_train)):
-#     plt.imshow(img_data_train[i])
-#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
-#     #plt.show()
-#
-# ##TEST##
-# #path
-#path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\test10'
-# images=os.listdir(path)
-# type(images)
-# len(images)
-#
-# img_data_test=[]
-# for img in images:
-#     img_arr=cv2.imread(os.path.join(path,img))
-#     img_data_test.append(img_arr)
-#
-# for i in range(len(img_data_test)):
-#     plt.imshow(img_data_test[i])
-#     #plt.imshow(img, cmap=plt.get_cmap('gray'))
-#     #plt.show()
-#
-##VALIDATION##
-path='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\val10'
-images=os.listdir(path)
+trainpath ='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\train10'
+images=os.listdir(trainpath)
 type(images)
 len(images)
 img_size = 224
 
 img_data_val=[]
 for img in images:
-    img_arr=cv2.imread(os.path.join(path,img))
-    img_data_val.append(img_arr)
+    for i in range(len(img_data_val)):
+        plt.imshow(img_data_val[i])
+        img = preprocess(img_data_val[i])
+        plt.imshow(img)
+        img = cv2.resize(img, (img_size, img_size))
+        plt.imshow(img, cmap=plt.get_cmap('gray'))
+        plt.axis("off")
+        #plt.show()
 
-for i in range(len(img_data_val)):
-    plt.imshow(img_data_val[i])
-    img = preprocess(img_data_val[i])
-    img = cv2.resize(img, (img_size, img_size))
-    plt.imshow(img, cmap=plt.get_cmap('gray'))
-    plt.axis("off")
-    #plt.show()
+##TEST##
+testpath='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\test10'
+images=os.listdir(testpath)
+type(images)
+len(images)
+img_size = 224
 
-filename='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\car'
-data =[]
-data = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
-#print(data)
+img_data_val=[]
+for img in images:
+    for i in range(len(img_data_val)):
+        plt.imshow(img_data_val[i])
+        img = preprocess(img_data_val[i])
+        plt.imshow(img)
+        img = cv2.resize(img, (img_size, img_size))
+        plt.imshow(img, cmap=plt.get_cmap('gray'))
+        plt.axis("off")
+        #plt.show()
 
-with open ('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json') as f:
-    data = json.load(f)
+##VALIDATION##
+valpath='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\val10'
+images=os.listdir(valpath)
+type(images)
+len(images)
+img_size = 224
 
-   # print (data)
-
-
-li = [item.get('labels') for item in data]
-#print(li)
-items = json.dumps(li)
-#print(type(items))
-
-for item in data:
-    for data_item in item['labels']:
-        data = (data_item['category'])
-        if 'car' in data :
-            a = item.get('name')
-            print(a)
-
-createFolder('./car/')
-cv2.imwrite('./car/', a)
-
-for box in data_item:
-    x1 = int(item['box2d']['x1'])
-    y1 = int(item['box2d']['y1'])
-    x2 = int(item['box2d']['x1'])
-    y2 = int(item['box2d']['y1'])
-    cropped_image = img[y1:y1 + y2, x1:x1 + x2]
-    x = np.append(x, preprocess(cropped_image))
+img_data_val=[]
+for img in images:
+    for i in range(len(img_data_val)):
+        plt.imshow(img_data_val[i])
+        img = preprocess(img_data_val[i])
+        plt.imshow(img)
+        img = cv2.resize(img, (img_size, img_size))
+        plt.imshow(img, cmap=plt.get_cmap('gray'))
+        plt.axis("off")
+        #plt.show()
 
 
 
+
+##LABELS##
+##TRAIN##
+train_labels = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_train.json')
+#print(train_labels.head())
+#print(train_labels.labels[0])
+train_labels['newlabels'] = train_labels['labels'].map(newlabels)
+print(train_labels)
+
+val_labels = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
+#print(val_labels.head())
+
+
+
+# filename='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\car'
+# data =[]
+# data = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
+# names = []
 #
-# for item in data:
-#     for data_item in item['labels']:
-#         print (data_item['id'])
-
-
-
+# for item in data["name"]:
+#     names.append(item)
+#     print(names)
+# #print(data)
+#
+# # with open ('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json') as f:
+# #     data = json.load(f)
+# #
+# #    # print (data)
+# #
+# #
+# # li = [item.get('labels') for item in data]
+# # #print(li)
+# # items = json.dumps(li)
+# # #print(type(items))
+# #
+# # for item in data:
+# #     for data_item in item['labels']:
+# #         data = (data_item['category'])
+# #         box = (data_item['box2d'])
+# #         if 'car' in data :
+# #             a = item.get('name')
+# #             print(a)
+# #
+# # # createFolder('./car/')
+# # # cv2.imwrite('./car/', a)
+# #
+# # for box2d in box:
+# #     x1 = int(box2d['x1'])
+# #     y1 = int(box2d['y1'])
+# #     x2 = int(box2d['x2'])
+# #     y2 = int(box2d['y2'])
+# #     cropped_image = img[y1:y1 + y2, x1:x1 + x2]
+# #     x = np.append(x, preprocess(cropped_image))
+# #
+# #
+#
+# #
+# # for item in data:
+# #     for data_item in item['labels']:
+# #         print (data_item['id'])
+#
+#
+#
