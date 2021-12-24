eb434a9519bd2eef13afcd05b65df94a66d22e95
Amara <amara1092@gmail.com>
2021-12-24 00:35:22 +0000

    Final push for the ca1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow.keras
 from keras.datasets import mnist
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
@@ -12,6 +13,7 @@ import keras
 import random
 import requests
 from PIL import Image
+from tqdm import tqdm
 
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
@@ -55,12 +57,41 @@ def newlabels(labels):
   return ','.join(updlabels)
 
 
+def le_net_model():
+    model = Sequential()
+    model.add(Conv2D(60, (5, 5), input_shape=(32, 32, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(30, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(43, activation='softmax'))
+    model.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def modified_model():
+    model = Sequential()
+    model.add(Conv2D(60, (5, 5), input_shape=(32, 32, 1), activation='relu'))
+    model.add(Conv2D(60, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(30, (3, 3), activation='relu'))
+    model.add(Conv2D(30, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(43, activation='softmax'))
+    model.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
 ##TRAIN##
 trainpath ='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\train10'
 images=os.listdir(trainpath)
 type(images)
 len(images)
-img_size = 224
+img_size = 74
 
 img_data_val=[]
 for img in images:
@@ -110,17 +141,14 @@ for img in images:
         #plt.show()
 
 
-
-
-##LABELS##
-##TRAIN##
+# ##LABELS##
+# ##TRAIN##
 train_labels= pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_train.json')
-#print(train_labels.head())
-#print(train_labels.labels[0])
 train_labels['newlabels'] = train_labels['labels'].map(newlabels)
 #print(train_labels)
 updlabels = ",".join(train_labels.newlabels).split(",")
 updlabels = list(set(updlabels))
+
 # for upd in updlabels:
 #     if 'drivable area' in upd:
 #         del upd ['drivable area']
@@ -129,94 +157,123 @@ updlabels = list(set(updlabels))
 #         del upd['lane']
 #         print ('success')
 #print(updlabels)
+
 datatest = train_labels.copy()
 for updated in updlabels:
     datatest[updated] = datatest['newlabels'].str.contains(updated)
     datatest[updated] = datatest[updated].astype(int)
 
-print("1")
-labels = list(datatest.columns.values)
-labels = labels[3:]
-print(labels)
-counts = []
-for label in labels:
-    counts.append((label, datatest[label].sum()))
-info = pd.DataFrame(counts, columns=['Labels', 'Occurrence'])
-info = info.sort_values(['Occurrence']).reset_index(drop=True)
-
-sns.set_style("darkgrid")
-plt.figure(figsize=(12,10))
-order= ['train','motor','rider','bike','bus', 'truck', 'person','traffic light',
-        'traffic sign', 'lane','drivable area', 'car']
-ax= sns.barplot(labels, datatest.iloc[:,3:].sum().values, order= order)
-
-plt.title("Image Classification Labels", fontsize=22)
-plt.ylabel('Total Occurrences', fontsize=20)
-plt.xlabel('Labels', fontsize=20)
-
-#adding the text labels
-rects = ax.patches
-labels = info['Occurrence']
-for rect, label in zip(rects, labels):
-    height = rect.get_height()
-    ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom', fontsize=16)
-plt.xticks(rotation=45, fontsize=16, ha='right')
-plt.yticks(fontsize=16)
-plt.show()
-
-val_labels = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
-#print(val_labels.head())
-
 ###Data Processing#####
 
+labelslist = list(datatest.columns.values)
+labelslist = labelslist[3:]
+print(labelslist)
+
+num = []
+for ll in labelslist:
+    num.append((ll, datatest[ll].sum()))
+stats = pd.DataFrame(num, columns=['Labels', 'Occurrence'])
+print("1")
+#stats = stats.sort_values(['Occurrence']).reset_index(drop=True)
+print(stats)
+rowSums = datatest.iloc[:, 3:].sum(axis=1)
+multiLl_counts = rowSums.value_counts()
+multiLl_counts = multiLl_counts.iloc[1:]
+label_count = pd.DataFrame(multiLl_counts, columns=['Total # of images']).rename_axis('# of Labels', axis=1)
+print(label_count)
+
+IMG_SIZE = (86,86)
+
+train_img_datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, zoom_range=0.2, shear_range=0.1, rotation_range=10)
+trainSet = train_img_datagen.flow_from_dataframe(datatest, directory=trainpath, x_col ='name', y_col = updlabels, class_mode = "multi_output", seed= 24, subset = 'training', batch_size=55888, target_size = IMG_SIZE)
+
+train_images, train_labels = next(trainSet)
+train_labels_ = np.array(train_labels).reshape(len(train_labels), -1).T
+train_img = train_images.reshape(train_images.shape[0], -1)
+m_train = train_img.shape[0]
+
+print ("Number of training samples: " + str(m_train))
+print ("train_images shape: " + str(train_images.shape))
+print ("train_img shape: " + str(train_img.shape))
+print ("train_labels_ shape: " + str(train_labels_.shape))
+model = modified_model()
+print(model.summary())
+
+# model_history = model.fit(train_img,train_labels_,epochs=15, batch_size=64,
+#                           validation_data=(val_img, val_labels_))
+# #model_report function to retrieve metrics
+# model_report(model, model_history, train_img, train_labels_, test_img, test_labels_)
+# val_labels = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
+# #print(val_labels.head())
 
 
-# filename='C:\\Users\\amara\\Downloads\\bdd100k_images_100k\\bdd100k\\images\\100k\\car'
-# data =[]
-# data = pd.read_json('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json')
-# names = []
+##CODE FOR SPLITTING THE TRAIN JSON INTO TEST##
+# with open(
+#         'C:\\Users\\smell\\Documents\\smarttech\\bdd100k_images_100k\\images\\100k\\bdd100k\\labels\\bdd100k_labels_images_train.json') as f:
+#     data = json.load(f)
+#     for i, x in enumerate(data):
+#         with open(str(i) + '.json', 'w') as f_out:
+#             json.dump(x, f_out)
+#             #                print(data)
+#             train_lables = data
 #
-# for item in data["name"]:
-#     names.append(item)
-#     print(names)
-# #print(data)
+# test_labels = train_test_split(train_labels, test_size=0.3)
+
+##Validation##
+
+data_val = pd.read_json('validation.json')
+#print(data_val)
+#data_val.info()
+
+data_val['updated_labels'] = data_val['labels'].map(newlabels)
+#print(data_val)
+
+category_list = ",".join(data_val.updated_labels).split(",")
+category_list = list(set(category_list))
+#print(category_list)
 #
-# # with open ('C:\\Users\\amara\\Downloads\\labels\\bdd100k\\labels\\bdd100k_labels_images_val.json') as f:
-# #     data = json.load(f)
-# #
-# #    # print (data)
-# #
-# #
-# # li = [item.get('labels') for item in data]
-# # #print(li)
-# # items = json.dumps(li)
-# # #print(type(items))
-# #
-# # for item in data:
-# #     for data_item in item['labels']:
-# #         data = (data_item['category'])
-# #         box = (data_item['box2d'])
-# #         if 'car' in data :
-# #             a = item.get('name')
-# #             print(a)
-# #
-# # # createFolder('./car/')
-# # # cv2.imwrite('./car/', a)
-# #
-# # for box2d in box:
-# #     x1 = int(box2d['x1'])
-# #     y1 = int(box2d['y1'])
-# #     x2 = int(box2d['x2'])
-# #     y2 = int(box2d['y2'])
-# #     cropped_image = img[y1:y1 + y2, x1:x1 + x2]
-# #     x = np.append(x, preprocess(cropped_image))
-# #
-# #
-#
-# #
-# # for item in data:
-# #     for data_item in item['labels']:
-# #         print (data_item['id'])
+data_validation = data_val.copy()
+for target in category_list:
+    data_validation[target] = data_validation['updated_labels'].str.contains(target)
+    data_validation[target] = data_validation[target].astype(int)
+    #print(data_validation)
+
+    labels = list(data_validation.columns.values)
+    labels = labels[3:]
+    #print(labels)
+
+    counts = []
+    for label in labels:
+        counts.append((label, data_validation[label].sum()))
+    dv_results= pd.DataFrame(counts, columns=['Labels', 'Appearance'])
+    #print(dv_results)
+
+    rowSums = data_validation.iloc[:, 3:].sum(axis=1)
+    multiLabels = rowSums.value_counts()
+    multiLabels = multiLabels.iloc[1:]
+    label_count = pd.DataFrame(multiLabels, columns=['Total # of images']).rename_axis('# of Labels', axis=1)
+    #print(label_count)
+
+
+## BOX2D attempt crop##
+# for item in data:
+#     for data_item in item['labels']:
+#         data = (data_item['category'])
+#         box = (data_item['box2d'])
+#         if 'car' in data :
+#             a = item.get('name')
+#             print(a)
 #
 #
+# for box2d in box:
+#     x1 = int(box2d['x1'])
+#     y1 = int(box2d['y1'])
+#     x2 = int(box2d['x2'])
+#     y2 = int(box2d['y2'])
+#     cropped_image = img[y1:y1 + y2, x1:x1 + x2]
+#     x = np.append(x, preprocess(cropped_image))
 #
+#
+
+
+
